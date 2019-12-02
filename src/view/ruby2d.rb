ec3e53068ae9291f9b56400013bccb5000b2c9c0
rioyi <wilbert.angarita@gmail.com>
2019-12-02 19:40:04 -0300

    remove square

--- src/view/ruby2d.rb
+++ src/view/ruby2d.rb
@@ -8,23 +8,27 @@ module View
       @pixel_size = 50
     end
 
-    def render(state)
+    def start(state)
       extend Ruby2D::DSL
       set(
         title: 'Snake',
         width: @pixel_size * state.grid.cols,
         height: @pixel_size * state.grid.rows)
+      show
+    end
+
+    def render(state)
       render_food(state)
       render_snake(state)
-      show
     end
 
     private
 
     def render_food(state)
+      @food.remove if @food
       extend Ruby2D::DSL
       food = state.food
-      Square.new(
+      @food = Square.new(
         x: food.col * @pixel_size,
         y: food.row * @pixel_size,
         size: @pixel_size,
@@ -33,9 +37,11 @@ module View
     end
 
     def render_snake(state)
+      @snake_positions.each(&:remove) if @snake_positions
+
       extend Ruby2D::DSL
       snake = state.snake
-      snake.positions.each do |pos|
+      @snake_positions = snake.positions.map do |pos|
         Square.new(
           x: pos.col * @pixel_size,
           y: pos.row * @pixel_size,
