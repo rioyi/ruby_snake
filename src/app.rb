09ffa44bc0e0c9ad48994709b526b2470cc28875
rioyi <wilbert.angarita@gmail.com>
2019-12-23 03:31:34 -0300

    Finalizando el juego y agregando mensaje de fin + puntos

--- src/app.rb
+++ src/app.rb
@@ -11,12 +11,18 @@ class App
 
   def start
     @view = View::Ruby2dView.new(self)
-    Thread.new { init_timer(@view) }
+    timer_thread = Thread.new { init_timer(@view) }
     @view.start(@state)
+    timer_thread.join
   end
 
   def init_timer(view)
     loop do
+      if @state.game_finished
+        puts 'Juego Finalizado'
+        puts "Puntaje: #{@state.snake.positions.length}"
+        break
+      end
       # trigger movement
       @state = Actions::move_snake(@state)
       @view.render(@state)
